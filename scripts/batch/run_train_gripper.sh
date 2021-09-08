55b858b81eccfdbdb6934a922c38a3c43ed25048
xuhuazhe <xuhuazhe12@gmail.com>
2021-09-08 16:38:19 -0700

    script update

--- scripts/batch/run_train_gripper.sh
+++ scripts/batch/run_train_gripper.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 cd /viscam/u/hxu/projects/deformable/VGPL-Dynamics-Prior
 
-declare -a arr=("emd")
-declare -a arr2=("10" "15" "20")
+declare -a arr=("emd_uh")
+declare -a arr2=("10")
+declare -a arr3=("0.1" "0.05" "0.02")
 for i in "${arr[@]}"
 do
     for j in "${arr2[@]}"
     do
-        echo "$i"
+        for k in "${arr3[@]}"
+        do
+            echo "$i"
 	    echo "$j"
-        sbatch ./scripts/batch/gripper_train.sh $i $j
+	    echo "$k"
+            sbatch ./scripts/batch/gripper_train.sh $i $j $k
+	done
     done
 done
