8e8f1b58db05f5c423685b2a0baf909848f439ee
Haochen Shi <hshi74@sc.stanford.edu>
2021-09-25 12:10:31 -0700

    Update the script

--- scripts/batch/gripper_train.sh
+++ scripts/batch/gripper_train.sh
@@ -7,11 +7,11 @@
 #SBATCH --output=/sailhome/hshi74/output/deformable/%A.out
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=10
-source ~/.bashrc
-conda activate deformable
+# source ~/.bashrc
+# conda activate deformable
 # export PYTHONPATH="/viscam/u/hshi74/projects/deformable/baselines:/viscam/u/hshi74/projects/deformable/PlasticineLab"
-cd /viscam/u/hshi74/projects/deformable/VGPL-Dynamics-Prior
-export LD_LIBRARY_PATH="/sailhome/hshi74/my_lib/:$LD_LIBRARY_PATH"
+# cd /viscam/u/hshi74/projects/deformable/VGPL-Dynamics-Prior
+# export LD_LIBRARY_PATH="/sailhome/hshi74/my_lib/:$LD_LIBRARY_PATH"
 
 
 bash scripts/dynamics/train_Gripper_dy.sh $1 $2 $3 $4
