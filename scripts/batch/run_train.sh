c4ef67c1271f3763ef4d0b5ac97878a114de0ae9
Haochen Shi <hshi74@sc.stanford.edu>
2021-10-22 10:45:45 -0700

    Update batch script

--- scripts/batch/run_train.sh
+++ scripts/batch/run_train.sh
@@ -2,11 +2,12 @@
 # cd /viscam/u/hxu/projects/deformable/VGPL-Dynamics-Prior
 # cd /viscam/u/hshi74/projects/deformable/VGPL-Dynamics-Prior
 
-declare -a arr=("5")
-declare -a arr2=("0.5")
-declare -a arr3=("0.5")
-declare -a arr4=("0.05")
-declare -a arr5=("0.05")
+declare -a arr=("L2Shape")
+declare -a arr2=("5")
+declare -a arr3=("0.0")
+declare -a arr4=("0.0")
+declare -a arr5=("0.0")
+declare -a arr6=("0.05")
 for i in "${arr[@]}"
 do
     for j in "${arr2[@]}"
@@ -16,10 +17,13 @@ do
             for l in "${arr4[@]}"
             do
                 for m in "${arr5[@]}"
-                do
-                    echo "$i $j $k $l $m"
-                    sbatch ./scripts/dynamics/train_dy.sh $i $j $k $l $m
-                    # bash ./scripts/dynamics/train_dy.sh $i $j $k $l $m
+                do 
+                    for n in "${arr6[@]}"
+                    do
+                        echo "$i $j $k $l $m $n"
+                        sbatch ./scripts/dynamics/train_dy.sh $i $j $k $l $m $n
+                        # bash ./scripts/dynamics/train_dy.sh $i $j $k $l $m $n
+                    done    
                 done
             done
 	    done
