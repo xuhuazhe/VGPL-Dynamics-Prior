3d9d27c37d26f0b56206358726222b9adcffe7be
xuhuazhe12 <xuhuazhe12@gmail.com>
2021-11-08 14:12:39 -0800

    Update train_dy.sh

--- scripts/dynamics/train_dy.sh
+++ scripts/dynamics/train_dy.sh
@@ -48,7 +48,8 @@ python train.py \
 	--n_rollout 50 \
 	--ckp_per_iter 10000 \
 	--curriculum 0 \
-	--noisy_his 1 \
+	--noisy_his 0 \
+	--pstep 3 \
 	--losstype $1 \
 	--sequence_length $2 \
 	--emd_weight $3 \
