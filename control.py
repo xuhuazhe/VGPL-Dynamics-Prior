bb5d8258b9ad204c4cdfdb194809210d49f19636
Haochen Shi <hshi74@stanford.edu>
2021-11-30 12:18:58 -0800

    Add the shape dataset

--- control.py
+++ control.py
@@ -1043,13 +1043,15 @@ def main():
         shape_dir = os.path.join(os.getcwd(), 'shapes', args.goal_shape_name)
         goal_shapes = []
         for i in range(args.n_grips):
-            goal_frame_name = f'{args.goal_shape_name}_{i}.h5'
-            if args.shape_aug:
-                goal_frame_name = 'shape_' + goal_frame_name
+            if args.subgoal:
+                goal_frame_name = f'{args.goal_shape_name}_{i}.h5'
+            else:
+                goal_frame_name = f'{args.goal_shape_name}.h5'
+            # if args.shape_aug:
+            #     goal_frame_name = 'shape_' + goal_frame_name
             goal_frame_path = os.path.join(shape_dir, goal_frame_name)
             goal_data = load_data(data_names, goal_frame_path)
             goal_shapes.append(torch.FloatTensor(goal_data[0]).unsqueeze(0)[:, :n_particle, :])
-        # goal_shapes = torch.stack(goal_shapes)
     else:
         goal_shapes = torch.FloatTensor(all_p[-1]).unsqueeze(0)[:, :n_particle, :]
 
--- shape.py
+++ shape.py
@@ -1,73 +1,109 @@
 from sdf import *
 from string import ascii_uppercase
 from plb.algorithms.sample_data import *
+from data_utils import load_data
+
+import matplotlib.pyplot as plt
+import open3d as o3d
+import torch
 import trimesh
 
-n_particles = 300
+n_particle_sample = 2000
+n_particle = 300
 
+floor_pos = np.array(
+    [[0.25, 0., 0.25], [0.25, 0., 0.5], [0.25, 0., 0.75],
+    [0.5, 0., 0.25], [0.5, 0., 0.5], [0.5, 0., 0.75],
+    [0.75, 0., 0.25], [0.75, 0., 0.5], [0.75, 0., 0.75]]
+)
+
+data_names = ['positions', 'shape_quats', 'scene_params']
+
+def visualize_points(all_points, n_particles, path):
+    # print(all_points.shape)
+    points = all_points[:n_particles]
+    shapes = all_points[n_particles:]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[:, 0], points[:, 2], points[:, 1], c='b', s=20)
+    ax.scatter(shapes[:, 0], shapes[:, 2], shapes[:, 1], c='r', s=20)
+    
+    extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
+    sz = extents[:, 1] - extents[:, 0]
+    centers = np.mean(extents, axis=1)
+    maxsize = max(abs(sz))
+    r = 0.25  # maxsize / 2
+    for ctr, dim in zip(centers, 'xyz'):
+        getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
+
+    plt.savefig(path)
+    # plt.show()
 
 if __name__ == "__main__":
-    FONT = '/usr/share/fonts/truetype/freefont/FreeSansBold.ttf'
-    
-    for c in ascii_uppercase:
-        w, h = measure_text(FONT, c)
-        f = text(FONT, c).scale(0.5).extrude(0.2).translate((0.5, 0.1, 0.5))
-        shape_name = f'shapes/alphabet/{c}.ply'
-        f.save(shape_name, step=0.01)
+    # FONT = '/usr/share/fonts/truetype/freefont/FreeSansBold.ttf'
 
-        tri_mesh = trimesh.load_mesh(shape_name)
+    # w, h = measure_image(IMAGE)
 
-        sampled_points = sampling(lower, upper, tri_mesh, [], n_particles)
+    # f = rounded_box((w * 1.1, h * 1.1, 0.1), 0.05)
+    # f |= image(IMAGE).extrude(1) & slab(z0=0, z1=0.075)
 
-        sampled_pcd = o3d.geometry.PointCloud()
-        sampled_pcd.points = o3d.utility.Vector3dVector(sampled_points)
+    update = True
+    debug = False
+    prefix = 'shapes'
+    image_names = ['butterfly', 'car', 'elephant', 'fish', 'flower', 'heart', 'house', 'panda', 'star']
+    for i, n in enumerate(image_names):
+        if debug and i > 0: break
 
-        # o3d.visualization.draw_geometries([sampled_pcd])
+        point_cloud_path = f'{prefix}/{n}/{n}.ply'
+        if not os.path.exists(point_cloud_path) or update:
+            image_path = f'{prefix}/{n}/{n}.png'
+            w, h = measure_image(image_path)
+            f = image(image_path).scale((0.33/w, 0.33/h)).extrude(0.175).orient(Y).translate((0.5, 0.1, 0.5))
+            f.save(point_cloud_path, step=0.01)
 
-        cl, inlier_ind = sampled_pcd.remove_statistical_outlier(nb_neighbors=40, std_ratio=1.5)
-        sampled_pcd_t = sampled_pcd.select_by_index(inlier_ind)
+        h5_path = f'{prefix}/{n}/{n}.h5'
 
-        # o3d.visualization.draw_geometries([sampled_pcd_t])
+        if not os.path.exists(h5_path) or update:
+            pcd = o3d.io.read_point_cloud(point_cloud_path)
+            upper = pcd.get_max_bound()
+            lower = pcd.get_min_bound()
 
-        # cl, inlier_ind = sampled_pcd_t.remove_radius_outlier(nb_points=10, radius=0.02)
-        # sampled_pcd_n = sampled_pcd_t.select_by_index(inlier_ind)
+            tri_mesh = trimesh.load_mesh(point_cloud_path)
 
-        # o3d.visualization.draw_geometries([sampled_pcd_n])
+            sampled_points = sampling(lower, upper, tri_mesh, [], n_particle_sample)
 
-        sampled_points = np.asarray(sampled_pcd_t.points)
+            sampled_pcd = o3d.geometry.PointCloud()
+            sampled_pcd.points = o3d.utility.Vector3dVector(sampled_points)
 
-        # voxels = mesh_to_sdf.mesh_to_voxels(tri_mesh, 64, pad=True)
-        # print(voxels)
+            # o3d.visualization.draw_geometries([sampled_pcd])
 
-        # vertices, faces, normals, _ = skimage.measure.marching_cubes(voxels, level=0)
-        # mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
-        # mesh.show()
+            cl, inlier_ind = sampled_pcd.remove_statistical_outlier(nb_neighbors=40, std_ratio=1.5)
+            sampled_pcd_t = sampled_pcd.select_by_index(inlier_ind)
 
-        # points, sdf = mesh_to_sdf.sample_sdf_near_surface(tri_mesh, surface_point_method='sample', number_of_points=250000)
-        # print(sdf)
+            # o3d.visualization.draw_geometries([sampled_pcd_t])
 
-        # sampled_points = points[sdf < 0, :]
+            # cl, inlier_ind = sampled_pcd_t.remove_radius_outlier(nb_points=10, radius=0.02)
+            # sampled_pcd_n = sampled_pcd_t.select_by_index(inlier_ind)
 
-        # surface_pcd = mesh_to_sdf.get_surface_point_cloud(tri_mesh)
+            # o3d.visualization.draw_geometries([sampled_pcd_n])
 
-        # # colors = np.zeros(points.shape)
-        # # colors[sdf < 0, 2] = 1
-        # # colors[sdf > 0, 0] = 1
-        # colors = np.zeros(sampled_points.shape)
-        # colors[:, 2] = 1
-        # cloud = pyrender.Mesh.from_points(sampled_points, colors=colors)
-        # py_mesh = pyrender.Mesh.from_trimesh(tri_mesh)
-        # # surface_mesh = pyrender.Mesh.from_points(surface_pcd.points)
-        # scene = pyrender.Scene()
-        # scene.add(cloud)
-        # scene.add(py_mesh)
-        # # scene.add(surface_mesh)
-        # viewer = pyrender.Viewer(scene, use_raymond_lighting=True, point_size=2)
+            sampled_points = np.asarray(sampled_pcd_t.points)
 
-        sampled_points = fps(sampled_points, K=k_fps_particles, partial=False)
-        # sampled_points.paint_uniform_color([0,0,0])
+            sampled_points = fps(sampled_points, K=n_particle, partial=False)
+            # sampled_points.paint_uniform_color([0,0,0])
+            
+            # sampled_pcd_fps = o3d.geometry.PointCloud()
+            # sampled_pcd_fps.points = o3d.utility.Vector3dVector(sampled_points)
+            # o3d.visualization.draw_geometries([sampled_pcd_fps])
+
+            positions = np.concatenate([sampled_points, floor_pos])
+            shape_quats = np.zeros((1, 4), dtype=np.float32)
+            data = [positions, shape_quats, scene_params]
+
+            store_data(data_names, data, h5_path)
+
         # import pdb; pdb.set_trace()
-        
-        # sampled_pcd_fps = o3d.geometry.PointCloud()
-        # sampled_pcd_fps.points = o3d.utility.Vector3dVector(sampled_points)
-        # o3d.visualization.draw_geometries([sampled_pcd_fps])
+        goal_data = load_data(data_names, h5_path)
+        goal_shape = torch.FloatTensor(goal_data[0]).unsqueeze(0)[:, :n_particle, :]
+        visualize_points(goal_data[0], n_particle, f'{prefix}/{n}/{n}_sampled')
