98471367b6c105fa826ec4d12f19cab939a92c19
xuhuazhe12 <xuhuazhe12@gmail.com>
2022-01-21 16:59:02 -0800

    Update control.py

--- control.py
+++ control.py
@@ -1072,8 +1072,8 @@ class Planner(object):
             mid_point_clipped_opt.append(mid_point_clipped)
             init_pose = get_pose(mid_point_clipped, angles[idx[-1], i])
             init_pose_seq_opt.append(init_pose)
-        import pdb; pdb.set_trace()
         init_pose_seq_opt = torch.stack(init_pose_seq_opt)
+        import pdb; pdb.set_trace()
 
         gripper_rate_opt = torch.clamp(gripper_rates[idx[-1]], min=0, max=task_params["gripper_rate_limits"][1])
         act_seq_opt = get_action_seq_from_pose(init_pose_seq_opt, gripper_rate_opt)
