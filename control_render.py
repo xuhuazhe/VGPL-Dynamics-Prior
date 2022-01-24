15fb33ff0d6c1a65aa77cd44c2e40dcd1e7ca088
xuhuazhe12 <xuhuazhe12@gmail.com>
2022-01-24 15:56:55 -0800

    Update control_render.py

--- control_render.py
+++ control_render.py
@@ -104,7 +104,7 @@ def main():
             env.render_cfg.camera_rot_4 = (0.8, 3.14)
 
         update_camera(env)
-        small_list = 'EFMNWZ'
+        small_list = 'EFMNZ'
         try:
             if args.goal_shape_name in 'VJ':
                 chosen_appendix = '3'
